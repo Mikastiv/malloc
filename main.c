7591cfe713d26a9dcad5901dc70d496fcf17362f
mikastiv <mikastiv@outlook.com>
2024-01-19 05:51:30 -0500

    better test

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include "memory.h"
 
+static void
+memoryset(void* dst, const size_t v, size_t size) {
+    char* dst_bytes = (char*)dst;
+
+    if (size > 32) {
+        size_t* dst_big = (size_t*)dst;
+
+        size_t blocks_count = size / sizeof(size_t);
+        size = size % sizeof(size_t);
+
+        for (size_t i = 0; i < blocks_count; ++i) {
+            dst_big[i] = v;
+        }
+
+        dst_bytes += blocks_count * sizeof(size_t);
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+        for (size_t i = 0; i < size; ++i) {
+            dst_bytes[i] = (char)v;
+        }
+    }
+}
+
 static size_t
 rotate_left(const size_t n, const size_t d) {
     static const size_t nbits = sizeof(size_t) * 8;
@@ -29,17 +53,22 @@ random(void) {
 int main() {
     void** blocks = malloc(256 * sizeof(void*));
     for (size_t i = 0; i < 128; i++) {
-        blocks[i] = malloc(random() % 8096);
+        const size_t size = random() % 8096;
+        blocks[i] = malloc(size);
+        memoryset(blocks[i], 0xAAAAAAAABBBBBBBBULL, size);
     }
     for (size_t i = 64; i < 128; i++) {
         free(blocks[i]);
     }
     for (size_t i = 64; i < 128; i++) {
-        blocks[i] = malloc(random() % 8096);
+        const size_t size = random() % 8096;
+        blocks[i] = malloc(size);
+        memoryset(blocks[i], 0xAAAAAAAABBBBBBBBULL, size);
     }
     show_alloc_mem();
     for (size_t i = 0; i < 128; i++) {
         free(blocks[i]);
     }
+    free(blocks);
     show_alloc_mem();
 }
